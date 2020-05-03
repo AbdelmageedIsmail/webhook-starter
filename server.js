9a4511f884be1f9b001bcccabe1c28830244bc83
AbdelmageedIsmail <20727134+AbdelmageedIsmail@users.noreply.github.com>
2020-05-03 15:41:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get("/", function (request, response) {
   response.send("<h1>WhatsApp Bot App</h1>\n<h1>Received messages</h1>\n"+receivedMessages+"<h1>Sent messages statuses</h1>\n"+messageStatuses+"\n<h1>Notifications</h1>"+notifications);
 });
 
+app.get("/read", function(req, res) {
+  // this does not exist
+  fs.createReadStream("my-self-esteem.txt");
+});
+
 app.get("/test", function (request, response) {
   
   
